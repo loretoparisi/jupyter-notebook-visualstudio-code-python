b12f1e454d689d88cd394cb5e6dcefb2dfb17adb
Loreto Parisi <loretoparisi@gmail.com>
2018-11-16 10:44:33 +0100

    added fastai progressbar example

---
+++ progressbar.py
@@ -0,0 +1,11 @@
+#%%
+from fastprogress import master_bar, progress_bar
+from time import sleep
+mb = master_bar(range(10))
+for i in mb:
+    for j in progress_bar(range(100), parent=mb):
+        sleep(0.01)
+        mb.child.comment = f'second bar stat'
+    mb.first_bar.comment = f'first bar stat'
+    mb.write(f'Finished loop {i}.')
+    #mb.update_graph(graphs, x_bounds, y_bounds)
